65ff14b7a4b231d6385781c96c81069bb0436b40
Tássyo Santana <tassyo.santana@hotmail.com>
2018-03-23 12:06:51 -0300

    Autenticação JWT + ASP.NET Core 2.0

---
+++ APIAlturas/User-TokenConfigurations.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIAlturas
+{
+    public class User
+    {
+        public string UserID { get; set; }
+        public string AccessKey { get; set; }
+    }
+
+    public class TokenConfigurations
+    {
+        public string Audience { get; set; }
+        public string Issuer { get; set; }
+        public int Seconds { get; set; }
+    }
+}
---
+++ APIAlturas/UsersDAO.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace APIAlturas
+{
+    public class UsersDAO
+    {
+        private IConfiguration _configuration;
+
+        public UsersDAO(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public User Find(string userID)
+        {
+            using (SqlConnection  conexao = new SqlConnection(_configuration.GetConnectionString("ExemploJWT")))
+            {
+                return conexao.QueryFirstOrDefault<User>("" +
+                   "SELECT UserID, AccessKey " +
+                    "FROM dbo.Users " +
+                    "WHERE UserID = @UserID", new { UserID = userID});
+            }
+        }
+    }
+}
